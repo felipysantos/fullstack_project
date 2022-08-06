fe200b707dabb486705f8f43e66cf4829e762464
felipysantos <felipys23@gmail.com>
2022-08-06 14:01:33 -0300

    updated: index and package.json from heroku deploy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,10 +3,12 @@ import appRouter from "./routes/routes";
 
 const app = express();
 
+const port = process.env.PORT || 3000
+
 app.use(express.json());
 
 app.use("/", appRouter);
 
-app.listen(3000);
+app.listen(port);
 
 export default app;
