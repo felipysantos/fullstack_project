510a62e301e618d4ddd0b6e0fba81d4a63df847c
felipysantos <felipys23@gmail.com>
2022-08-08 14:38:53 -0300

    update: index.js

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -5,9 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const routes_1 = __importDefault(require("./routes/routes"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3333;
 app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 app.use("/", routes_1.default);
-app.listen(port);
+app.listen(port, () => {
+    console.log(`App rodando ${port}`);
+});
 exports.default = app;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,18 @@
 import express from "express";
 import appRouter from "./routes/routes";
+import cors from "cors"
 
 const app = express();
 
-const port = process.env.PORT || 3333
+const port = process.env.PORT || 3333;
 
 app.use(express.json());
 
+app.use(cors())
 app.use("/", appRouter);
 
-app.listen(port);
+app.listen(port, () => {
+  console.log(`App rodando ${port}`);
+});
 
 export default app;
